49a389dcb27ac5e159bf4a5cd71dde901e7685f9
Gracie Ingermanson <gmi@umich.edu>
2020-11-19 12:47:49 -0500

    Added model definitions using sequelize

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 const express = require("express");
 const app = express();
+const { db, Page, User } = require('./models');
 const morgan = require("morgan");
 const path = require("path");
 const layout = require("./views/layout");
+const PORT = 3000;
+
+db.authenticate()
+  .then(() => {
+    console.log('connected to the database');
+  })
 
 const staticMiddleware = express.static(path.join(__dirname, "/public"));
 
@@ -14,4 +21,11 @@ app.get("/", (req, res) => {
   res.send(layout("hello"));
 });
 
-app.listen(3000);
+async function syncDb() {
+  await db.sync();
+  app.listen(PORT);
+}
+
+syncDb();
+
+
