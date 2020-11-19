ea23dfb8f4cd3aede9f2a0a17cfb9db06160c2c2
Katie Weinstein <katie.weinstein3@gmail.com>
2020-11-19 17:00:51 -0500

    attempted to make slug converter

--- models/index.js
+++ models/index.js
@@ -1,19 +1,36 @@
-const Sequelize = require('sequelize');
-const db = new Sequelize('postgres://localhost:5432/wikistack', {logging: false});
-const validator = require('validator');
+const Sequelize = require("sequelize");
+const db = new Sequelize("postgres://localhost:5432/wikistack", {
+  logging: false,
+});
+const validator = require("validator");
 
-const Page = db.define('page', {
-  title: {type: Sequelize.STRING, allowNull: false},
-  slug: {type: Sequelize.STRING, allowNull: false},
-  content: {type: Sequelize.STRING, allowNull: false},
-  status: Sequelize.ENUM('open', 'closed')
+const Page = db.define("page", {
+  title: { type: Sequelize.STRING, allowNull: false },
+  slug: { type: Sequelize.STRING, allowNull: false },
+  content: { type: Sequelize.STRING, allowNull: false },
+  status: Sequelize.ENUM("open", "closed"),
 });
 
-const User = db.define('user', {
-  name: {type: Sequelize.STRING, allowNull: false},
-  email: {type: Sequelize.STRING, allowNull: false, unique: true, validate: {isEmail: true}}
-})
+const User = db.define("user", {
+  name: { type: Sequelize.STRING, allowNull: false },
+  email: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    unique: true,
+    validate: { isEmail: true },
+  },
+});
+
+function urlify(string) {
+  string = string.replace(/\s+/g, "_").replace(/\W/g, "");
+}
+
+Page.beforeCreate((pageInstance) => {
+  pageInstance.slug = urlify(pageInstance.title);
+});
 
 module.exports = {
-  db, Page, User
-}
+  db,
+  Page,
+  User,
+};
--- routes/wiki.js
+++ routes/wiki.js
@@ -2,7 +2,7 @@ const express = require("express");
 const wiki = express.Router();
 const main = require("../views/main");
 const addPage = require("../views/addPage");
-const {db, Page, User} = require('../models/index')
+const { db, Page, User } = require("../models/index");
 
 wiki.get("/", (req, res, next) => {
   try {
@@ -21,22 +21,20 @@ wiki.get("/add", (req, res, next) => {
 });
 
 wiki.post("/", async (req, res, next) => {
-
   //res.json(req.body);
   try {
-
-    const urlifyTitle = req.body.title.trim().split(' ').join('%20');
+    //const urlifyTitle = req.body.title.trim().split(" ").join("%20");
     const page = await Page.create({
       title: req.body.title,
       content: req.body.content,
-      slug:`localhost:3000${urlifyTitle}`
+      slug: req.body.title,
     });
 
     // make sure we only redirect *after* our save is complete! Don't forget to `await` the previous step. `create` returns a Promise.
-    res.redirect('/');
-  } catch (error) { next(error) }
-
-
+    res.redirect("/");
+  } catch (error) {
+    next(error);
+  }
 });
 
 module.exports = wiki;
